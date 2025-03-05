dd83322478c9994a7f01fd2b5fe1ac7a3bba5edb
Fred Wan <mail@fredwan.net>
2025-03-05 17:19:52 +0100

    Added build and docker files for insect containers in directory app

---
+++ app/build_pingerdaemon_APOLLO.sh
@@ -0,0 +1,7 @@
+cp ../target/pingerdaemon-rabbitmq-client-0.0.1-SNAPSHOT.jar ./
+docker build -t pingerdaemon-rabbitmq-apollo -f ./dockerfile_pingerdaemon_APOLLO .
+docker run -d 	--name APOLLO  \
+		--network=fedsky \
+		--ip=192.168.50.227 \
+		pingerdaemon-rabbitmq-apollo
+
--- app/build_pingerdaemon_CAPTUW.sh
+++
@@ -1,7 +0,0 @@
-cp ../target/pingerdaemon-rabbitmq-client-0.0.1-SNAPSHOT.jar ./
-docker build -t pingerdaemon-rabbitmq-client -f ./dockerfile_pingerdaemon_entrypoint_CAPTUW .
-docker run -d 	--name pingerdaemon-CAPTUW  \
-		--network=fedsky \
-		--ip=192.168.50.227 \
-		pingerdaemon-rabbitmq-client 
-
