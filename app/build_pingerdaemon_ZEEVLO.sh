e1f3bf9fa7a1c213fed548c894f6730b2dbd12a6
Fred Wan <mail@fredwan.net>
2025-03-07 22:37:28 +0100

    Fixed wrong IP address ZEEVLO

--- app/build_pingerdaemon_ZEEVLO.sh
+++ app/build_pingerdaemon_ZEEVLO.sh
@@ -2,6 +2,6 @@ cp ../target/pingerdaemon-rabbitmq-client-0.0.1-SNAPSHOT.jar ./
 docker build -t pingerdaemon-rabbitmq-zeevlo -f ./dockerfile_pingerdaemon_ZEEVLO .
 docker run -d 	--name "ZEEVLO"  \
 		--network=fedsky \
-		--ip=192.168.50.233 \
+		--ip=192.168.50.234 \
 		pingerdaemon-rabbitmq-zeevlo
 
