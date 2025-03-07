a8cbbbd5cd582f2d9ef7d1c33e932501728fafdd
Fred Wan <mail@fredwan.net>
2025-03-07 18:31:05 +0100

    Added build script to build all containers

---
+++ app/build_all_continers.sh
@@ -0,0 +1,8 @@
+./build_pingerdaemon_APOLLO.sh;
+./build_pingerdaemon_BELYTA.sh;
+./build_pingerdaemon_CICADE.sh;
+./build_pingerdaemon_HOMMEL.sh;
+./build_pingerdaemon_KREKEL.sh;
+./build_pingerdaemon_OERMOT.sh;
+./build_pingerdaemon_WITOOG.sh;
+./build_pingerdaemon_ZEEVLO.sh
--- app/build_pingerdaemon_ZEEVLO.sh
+++ app/build_pingerdaemon_ZEEVLO.sh
@@ -1,6 +1,6 @@
 cp ../target/pingerdaemon-rabbitmq-client-0.0.1-SNAPSHOT.jar ./
 docker build -t pingerdaemon-rabbitmq-zeevlo -f ./dockerfile_pingerdaemon_ZEEVLO .
-docker run -d 	--name "WITOOG"  \
+docker run -d 	--name "ZEEVLO"  \
 		--network=fedsky \
 		--ip=192.168.50.233 \
 		pingerdaemon-rabbitmq-zeevlo
