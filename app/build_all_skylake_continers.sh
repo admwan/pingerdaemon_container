5ffe5f0bed62f118d5961a636182abcc1c75b92a
Fred Wan <mail@fredwan.net>
2025-03-14 17:19:22 +0100

    Added some container management scritps

--- app/build_all_skylake_continers.sh
+++
@@ -1,8 +0,0 @@
-./build_pingerdaemon_APOLLO_skylake.sh;
-./build_pingerdaemon_BELYTA_skylake.sh;
-./build_pingerdaemon_CICADE_skylake.sh;
-./build_pingerdaemon_HOMMEL_skylake.sh;
-./build_pingerdaemon_KREKEL_skylake.sh;
-./build_pingerdaemon_OERMOT_skylake.sh;
-./build_pingerdaemon_WITOOG_skylake.sh;
-./build_pingerdaemon_ZEEVLO_skylake.sh
---
+++ app/build_all_skylake_insect_continers.sh
@@ -0,0 +1,8 @@
+./build_pingerdaemon_APOLLO_skylake.sh;
+./build_pingerdaemon_BELYTA_skylake.sh;
+./build_pingerdaemon_CICADE_skylake.sh;
+./build_pingerdaemon_HOMMEL_skylake.sh;
+./build_pingerdaemon_KREKEL_skylake.sh;
+./build_pingerdaemon_OERMOT_skylake.sh;
+./build_pingerdaemon_WITOOG_skylake.sh;
+./build_pingerdaemon_ZEEVLO_skylake.sh
